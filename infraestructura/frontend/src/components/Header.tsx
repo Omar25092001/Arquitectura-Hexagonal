e363c01d213c4073ca383d5d8b0e3c6254588869
Omar25092001 <116413675+Omar25092001@users.noreply.github.com>
2025-06-10 20:53:02 -0400

    feat(front): logout

--- infraestructura/frontend/src/components/Header.tsx
+++ infraestructura/frontend/src/components/Header.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { IoMdLogOut } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Aquí puedes agregar la lógica para cerrar sesión, como limpiar el estado de autenticación
+    console.log('Deslogueado');
+    navigate('/'); // Redirigir a la página de inicio o login
+  }
   return (
     <header className="flex justify-between items-center px-5 py-5">
       <div className="text-white font-bold text-2xl">Configuración del Digital Twin</div>   
       <div className="flex items-center gap-4">
         <span className="text-white font-medium text-sm">Desloguearse</span>
-        <button className="text-white hover:text-gray-300 transition-colors">
+        <button 
+          onClick={handleLogout}
+          className="text-white hover:text-gray-300 transition-colors">
           <IoMdLogOut className="w-6 h-6" />
         </button>
       </div>
